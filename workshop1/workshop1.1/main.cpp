b205765a95c21f680efd5faf664e4dd6e8183de5
Kostya Romanov <kostya.romanov.2012@bk.ru>
2018-01-10 22:05:49 +0300

    C++

--- workshop1/workshop1.1/main.cpp
+++ workshop1/workshop1.1/main.cpp
@@ -1,32 +1,40 @@
 #include <SFML/Graphics.hpp>
-#include <SFML/System.hpp>
 #include <SFML/Window.hpp>
-#include <iostream>
+#include <SFML/System.hpp>
 
 int main()
-
 {
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 8;
 
-    sf::RenderWindow window(sf::VideoMode({200,200}),"pointer");
-    
-    sf::ConvexShape pointer;
-    pointer.setPointCount(7);
-    pointer.setPoint(0,{150,100});
-    pointer.setPoint(1,{135,100});
-    pointer.setPoint(2,{135,60});
-    pointer.setPoint(3,{95,60});
-    pointer.setPoint(4,{95,100});
-    pointer.setPoint(5,{80,100});
-    pointer.setPoint(6,{115,140});
-    pointer.setFillColor(sf::Color(255, 255, 0));
-    pointer.setOutlineColor(sf::Color(0,0,0));
-    pointer.setOutlineThickness(float(2));
-    
+    sf::RenderWindow window(sf::VideoMode({200, 300}), "Arrow");
+    window.clear(sf::Color(255, 255, 255));
+
+    sf::ConvexShape arrow;
+    arrow.setFillColor(sf::Color(255, 255, 0));
+    arrow.setOutlineThickness(3);
+    arrow.setOutlineColor(sf::Color(0, 0, 0));
+    arrow.setPosition(100, 100);
+    arrow.setPointCount(7);
+    arrow.setPoint(0, {0, -50});
+    arrow.setPoint(1, {50, 0});
+    arrow.setPoint(2, {30, 0});
+    arrow.setPoint(3, {30, 50});
+    arrow.setPoint(4, {-30, 50});
+    arrow.setPoint(5, {-30, 0});
+    arrow.setPoint(6, {-50, 0});
+    window.draw(arrow);
+    window.display();
+
     while (window.isOpen())
     {
-        window.clear(sf::Color(225, 225, 225));
-        window.draw(pointer);
-        window.display();
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
     }
 }
-
